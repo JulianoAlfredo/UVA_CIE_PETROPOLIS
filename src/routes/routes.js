7dc87092543a0ab0be751e3bf541bb94f75050a6
Anjinho <65797644+Anjinhuh@users.noreply.github.com>
2022-04-19 01:21:06 -0300

    ':sparkles:MAIN_PAGE_CREATED'

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,11 +4,13 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
 import login from '../pages/login/login'
 import signUp from '../pages/signUp/signUp'
+import MainPage from '../pages/mainPage/main'
 
 export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
+                <Route path='/' exact component={MainPage} />
                 <Route path="/login" exact component={login} />
                 <Route path='/cadastrar' exact component={signUp} />
             </Switch>
